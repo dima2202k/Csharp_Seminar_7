82795517073d4636d2c4a97c88e7910f693194b7
dmitriy komov <dima22.02.kom@mail.ru>
2024-05-05 16:39:43 +0300

    add ask_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задание 3
+
+void main ()
+{
+    int [] arrayNumber = [3, 6, 8, 1, 6, 0];
+    printNumber(arrayNumber, arrayNumber.Length - 1);
+}
+
+void printNumber (int[] arrayNumber, int counter)
+{
+    if (counter < 0)
+    {
+        return;      
+    }
+    System.Console.Write($"{arrayNumber[counter]} ");
+
+    printNumber(arrayNumber, counter - 1);
+}
+
+main();

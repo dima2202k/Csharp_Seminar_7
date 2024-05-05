c6a14696fd968329997180f9364fe5d17fe89cc8
dmitriy komov <dima22.02.kom@mail.ru>
2024-05-05 10:55:48 +0300

    add Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задание 1
+
+void main(int m, int n)
+{
+    int max = m;
+    int min = n;
+
+    if (m > n)
+    {
+        max =  m; 
+        min = n; 
+    } else
+    {
+        max = n;
+        min = m;
+    }
+    printNatural(max, min);
+}
+
+void printNatural(int max, int min)
+{
+    if (min == max - 1) return;    
+    System.Console.Write($"{min + 1} ");
+    printNatural(max, min + 1);
+}
+
+int m = 12;
+int n = 57;
+main(m, n);

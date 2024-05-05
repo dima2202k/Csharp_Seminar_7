23ca7623e22d68734910eb65c7c83fd66d80facc
dmitriy komov <dima22.02.kom@mail.ru>
2024-05-05 12:04:20 +0300

    add Task-002

---
+++ Task_002/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задание 2
+
+void main ()
+{
+    int n = 1;
+    int m = 1;
+    System.Console.WriteLine(calculateAkkerman(n, m));
+}
+
+int calculateAkkerman(int n, int m) 
+{
+    if (n == 0)  
+    {
+        return  m += 1;
+        
+    } else if (m == 0)
+    {
+        calculateAkkerman(n - 1, 1);  
+
+    } else if (m != 0 && n != 0)
+    {
+        calculateAkkerman(n - 1, calculateAkkerman(n, m - 1));
+    }
+
+    return(m);
+
+}
+
+main();
